4e6364efb12de0ed34876c0a23d1b717c3381b8f
Hjr <john789811@gmail.com>
2025-03-12 18:51:29 +1100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let userMessage = null; // Variable to store user's message
 const inputInitHeight = chatInput.scrollHeight;
 
 // API configuration
-const API_KEY = "PASTE-YOUR-API-KEY"; // Your API key here
+const API_KEY = "AIzaSyAPc9A0rAGh1SLR0Ny2TMg-CqyZe4A6Ji0"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const createChatLi = (message, className) => {
