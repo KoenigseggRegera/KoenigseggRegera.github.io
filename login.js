dcba070599dda01d9ebf8a5d9028ca46dd2beb41
Hjr <john789811@gmail.com>
2025-03-14 11:12:40 +1100

    Update login.js

--- login.js
+++ login.js
@@ -2,6 +2,7 @@ document.querySelector("#rizz").addEventListener("submit", (e) => {
     e.preventDefault();
     const val = document.querySelector("#code").value;
     if (val == "balling") {
+                localStorage.login = "iloveyou";
         window.location.href = "https://koenigseggregera.github.io/hub.html";
     }
 });
