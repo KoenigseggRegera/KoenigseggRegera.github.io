cb36340e0df3ac0cd30b559bd5416505f449eeb6
Hjr <john789811@gmail.com>
2025-03-27 10:24:29 +1100

    Update photomedia.js

--- photomedia.js
+++ photomedia.js
@@ -1,25 +1,6 @@
-// List of images with captions
 const images = [
     { filename: "image1.jpg", caption: "Beautiful Sunset" },
     { filename: "image2.jpg", caption: "Mountain View" },
-    { filename: "image3.jpg", caption: "Ocean Waves" }
+    { filename: "image3.jpg", caption: "Ocean Waves" },
+    { filename: "chili-pepper.png", caption: "Spicy Red Chili 🌶️" } // Added chili pepper image
 ];
-
-const gallery = document.getElementById("gallery");
-
-// Loop through images and add them to the gallery
-images.forEach(image => {
-    const container = document.createElement("div");
-    container.classList.add("image-container");
-
-    const imgElement = document.createElement("img");
-    imgElement.src = `images/${image.filename}`;
-    imgElement.alt = image.caption;
-
-    const captionElement = document.createElement("p");
-    captionElement.innerText = image.caption;
-
-    container.appendChild(imgElement);
-    container.appendChild(captionElement);
-    gallery.appendChild(container);
-});
