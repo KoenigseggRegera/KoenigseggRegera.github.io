38a0b21f2836297a0b00b7871baaed6b1ff0e91c
Hjr <john789811@gmail.com>
2025-03-27 10:29:21 +1100

    Update photomedia.js

--- photomedia.js
+++ photomedia.js
@@ -2,5 +2,5 @@ const images = [
     { filename: "image1.jpg", caption: "Beautiful Sunset" },
     { filename: "image2.jpg", caption: "Mountain View" },
     { filename: "image3.jpg", caption: "Ocean Waves" },
-    { filename: "chili-pepper.png", caption: "Spicy Red Chili 🌶️" } // Added chili pepper image
+    { filename: "red-hot-chili-pepper-isolated-transparent-white-background-png-clipart_888962-231.png", caption: "Spicy Red Chili 🌶️" } // Added chili pepper image
 ];
