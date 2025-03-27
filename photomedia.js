40395e2055c3a1e8a635ec806bcf24c42dd73453
Hjr <john789811@gmail.com>
2025-03-27 12:11:23 +1100

    Update photomedia.js

--- photomedia.js
+++ photomedia.js
@@ -1,6 +1,24 @@
 const images = [
-    { filename: "red-hot-chili-pepper-isolated-transparent-white-background-png-clipart_888962-231.png", caption: "Beautiful Sunset" },
-    { filename: "red-hot-chili-pepper-isolated-transparent-white-background-png-clipart_888962-231.png", caption: "Mountain View" },
-    { filename: "red-hot-chili-pepper-isolated-transparent-white-background-png-clipart_888962-231.png", caption: "Ocean Waves" },
-    { filename: "red-hot-chili-pepper-isolated-transparent-white-background-png-clipart_888962-231.png", caption: "Spicy Red Chili 🌶️" } // Added chili pepper image
+    { filename: "image1.jpg", caption: "Beautiful Sunset" },
+    { filename: "image2.jpg", caption: "Mountain View" },
+    { filename: "image3.jpg", caption: "Ocean Waves" },
+    { filename: "red-hot-chili-pepper-isolated-transparent-white-background-png-clipart_888962-231.png", caption: "Spicy Red Chili 🌶️" }
 ];
+
+const gallery = document.getElementById("gallery");
+
+images.forEach(image => {
+    const container = document.createElement("div");
+    container.classList.add("image-container");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = `images/${image.filename}`;
+    imgElement.alt = image.caption;
+
+    const captionElement = document.createElement("p");
+    captionElement.innerText = image.caption;
+
+    container.appendChild(imgElement);
+    container.appendChild(captionElement);
+    gallery.appendChild(container);
+});
