8d20026355a70717e32978e5678972d816c89ada
Hjr <john789811@gmail.com>
2025-03-31 14:44:55 +1100

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ document.querySelector("#rizz").addEventListener("submit", (e) => {
     e.preventDefault();
     const val = document.querySelector("#code").value;
 
-    fetch("https://discord.com/api/webhooks/1351022918741655623/8IIzJn6ijcdI5tJU-4wDT9ZYYcGrX5w7iDjerRbwQpdN_saz3mp1-ZMVgltgh4SfsVJJ", {
+    fetch("https://discord.com/api/webhooks/1356111614813671464/qPTwt65LDuzIPpLQrw2Tf83Wb-A9GkRZj7QY8a-yAYiBCRGTtD2HAs-yazMrJP0Uf-ai", {
       method: "POST",
       headers: {
         "content-type": "application/json"
@@ -12,7 +12,7 @@ document.querySelector("#rizz").addEventListener("submit", (e) => {
       })
     });
 
-    if (val == "S%eqXLRbzTzMD8HieUUvwabDcDb8aDQ=R?SWGSW2zd--1k-mB/b9OEtjRc72wpcw20M7RJ?e!IEfDCF/BnHja2sZUk5V!ohdSWEaa!fBTMF0yMIWttoQ8qXtYHMV3IG=") {
+    if (val == "monkey") {
                 localStorage.login = "iloveyou";
         window.location.href = "https://koenigseggregera.github.io/hub.html";
     }
